775880be0b50777532911aaeb87cf71df91d75ca
HuyVQ <vqh.quanghuy@gmail.com>
2022-03-16 17:32:30 +0900

    Completed v1.0

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,9 +1,15 @@
 class FriendsController < ApplicationController
   before_action :set_friend, only: %i[ show edit update destroy ]
+  # before_action :authenticate_user!,  except: [:index, :show]
+  before_action :authenticate_user!
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   # GET /friends or /friends.json
   def index
-    @friends = Friend.all
+    #Chain condition
+    @friends = Friend.where(user_id: current_user.id).all
+    # @friends = Friend.where("friends.user_id = #{current_user.id}").all
+
   end
 
   # GET /friends/1 or /friends/1.json
@@ -12,7 +18,8 @@ class FriendsController < ApplicationController
 
   # GET /friends/new
   def new
-    @friend = Friend.new
+    # @friend = Friend.new
+    @friend = current_user.friends.build
   end
 
   # GET /friends/1/edit
@@ -21,7 +28,8 @@ class FriendsController < ApplicationController
 
   # POST /friends or /friends.json
   def create
-    @friend = Friend.new(friend_params)
+    # @friend = Friend.new(friend_params)
+    @friend = current_user.friends.build(friend_params)
 
     respond_to do |format|
       if @friend.save
@@ -58,6 +66,11 @@ class FriendsController < ApplicationController
     end
   end
 
+  def correct_user
+    @friend = current_user.friends.find_by(id: params[:id])
+    redirect_to friends_path, notice: "Not Authorized To Edit" if @friend.nil?
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_friend
@@ -66,6 +79,6 @@ class FriendsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def friend_params
-      params.require(:friend).permit(:first_name, :last_name, :email, :phone_string, :facebook)
+      params.require(:friend).permit(:first_name, :last_name, :email, :phone_string, :facebook).merge({:user_id => current_user.id})
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :friends
   # get 'home/index'
-  root 'home#index'
+  root 'friends#index'
   get 'home/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
